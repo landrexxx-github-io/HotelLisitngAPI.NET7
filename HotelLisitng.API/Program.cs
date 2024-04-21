cfd075da559222482860a3ba50a9453e32c5f15e
Landrex Rebuera <landrex.rebuera@gmail.com>
2024-04-21 13:20:28 +0800

    Add project files.

---
+++ HotelLisitng.API/Program.cs
@@ -0,0 +1,40 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options => {
+    options.AddPolicy("ALLOW_ALL", 
+        policy => policy.AllowAnyHeader()
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod());
+});
+
+builder.Host.UseSerilog((ctx, config) => config.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+
+app.UseCors("ALLOW_ALL");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
